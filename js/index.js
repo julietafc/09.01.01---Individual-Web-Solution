f6af9f738ab6f351ef0bac11ad0dea4d96a5e3ba
Julieta Fernandez <juli47g1@stud.kea.dk>
2021-04-11 17:01:49 +0200

    images in homepage

--- js/index.js
+++ js/index.js
@@ -30,6 +30,16 @@ function showPosts(posts) {
     copy.querySelector("h2").textContent = post.title;
     copy.querySelector("h3 span").textContent = post.username;
     copy.querySelector("a.readmore").href = `article.html?article=${post._id}`;
+
+    if (post.imageUrl) {
+      imageArt = post.imageUrl;
+    } else {
+      imageArt =
+        "https://images.unsplash.com/photo-1615941341759-ab7d5188a846?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8aW5kb29yJTIwcGxhbnR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60";
+    }
+
+    copy.querySelector(".imgPost").src = imageArt;
+
     //apend
     document.querySelector(".articlesList").appendChild(copy);
   });
